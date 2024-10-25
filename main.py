c867fe031ba2c87a99a09a31dd477a2986197764
Ivosaja <ivoliobel@gmail.com>
2024-10-25 15:03:53 -0300

    Adding a new function

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ def multipliar_numeros(n1, n2):
 
 print(sumar_numeros(5,5))
 print(multipliar_numeros(2,8))
+
+def saludar_nahuel_gay(mensaje: str):
+    print(f"Hola nahuel, sos {mensaje}")
+
+saludar_nahuel_gay("GAY")
