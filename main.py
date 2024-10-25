a820d614631c04ae60d9b6060840a6e567a44376
NsRomano <romanonahuel24@gmail.com>
2024-10-25 14:55:45 -0300

    function new

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ print("test")
 def sumar_numeros(numero1, numero2):
     return numero1 + numero2
 
+def multipliar_numeros(n1, n2):
+    return n1 * n2
+
 print(sumar_numeros(5,5))
+print(multipliar_numeros(2,8))
